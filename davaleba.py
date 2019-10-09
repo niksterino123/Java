4359681bf337d0665d50b54cbb6758d2674c328c
niksterino123 <48384956+niksterino123@users.noreply.github.com>
2019-10-09 18:19:39 +0400

    Create davaleba.py

---
+++ davaleba.py
@@ -0,0 +1,10 @@
+kilometres = int(input('Enter the number of kilometres you wish to convert: '))
+metres = int(input('Enter the number of metres you wish to convert: '))
+
+metres = kilometres / 0.001
+
+print(kilometres, 'km converts to', metres, 'metres.')
+
+kilometres = metres * 0.001
+
+print(metres, 'metres converts to', kilometres ,'kilometres.')
